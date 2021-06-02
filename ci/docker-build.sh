5de9cc634f31fc360de60c58b68585075a0a7a8e
Jesse N <jesse@keplerdev.com>
2021-06-02 02:13:35 -0400

    Added GitHub docker build action

--- ci/docker-build.sh
+++ ci/docker-build.sh
@@ -1,26 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 zparseopts -A zopts -image-version: -alpine-version:: -openssh-version::
-
+ 
 local image_version="$zopts[--image-version]"
-local apine_version="$zopts[--alpine-version]"
+local alpine_version="$zopts[--alpine-version]"
 local openssh_version="$zopts[--openssh-version]"
 
 if [[ -n "${alpine_version} "]];
 then
-    exit 1;
+    alpine_version="3.13.5"
 fi
 
-if [[ -n $"{openssh_version}" ]];
+if [[ -n "${openssh_version}" ]];
 then
-    exit 1;
+    openssh_version="8.1_p1-r0"
 fi
 
 docker build . \
     -f ./amd64/Dockerfile \
     -t jessenich91/alpine-base:latest  \
     -t jessenich91/alpine-base:glibc-latest \
-    -t jessenich91/alpine-base:glibc-0.0.2-alpha \
-    -t jessenich91/alpine-base:alpine-3.13.5-glibc-0.0.2-alpha \
-    --build-arg "ALPINE_VERSION=3.13.5" \
-    --build-arg "OPENSSH_VERSION=8.1_p1-r0"
+    -t jessenich91/alpine-base:glibc-"${image_version}" \
+    -t jessenich91/alpine-base:alpine-"${alpine-version}"-glibc-"${image_version}" \
+    --build-arg "ALPINE_VERSION=${alpine_version}" \
+    --build-arg "OPENSSH_VERSION=${openssh_version}"
