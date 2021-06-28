e44e5606d16ca3731d5067593079efb4c6b4ca5d
Jesse N <jesse@keplerdev.com>
2021-06-27 21:13:04 -0700

    Changed tag vairants to be simpler and self explanatory.

--- Dockerfile
+++ Dockerfile
@@ -20,87 +20,93 @@
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ## SOFTWARE.
 
-ARG ALPINE_VERSION=
+ARG ALPINE_VERSION="latest"
 
 FROM alpine:"${ALPINE_VERSION:-latest}" as deps
 
+ARG NO_DOCS=
+ARG TZ=
+ARG USER_LOGIN_SHELL=
+ARG USER_LOGIN_FALLBACK_SHELL=
+
 LABEL maintainer="Jesse N. <jesse@keplerdev.com>"
 
-ENV USER_LOGIN_SHELL="/bin/zsh" \
-    USER_LOGIN_SHELL_FALLBACK="/bin/ash" \
-    TZ="America/NewYork" \
-    RUNNING_IN_DOCKER="true"
-    
-RUN apk update
+ENV ALPINE_VERSION=${ALPINE_VERSION} \
+    NO_DOCS="${NO_DOCS:+true}" \
+    USER_LOGIN_SHELL="${USER_LOGIN_SHELL:-/bin/zsh}" \
+    USER_LOGIN_SHELL_FALLBACK="${USER_LOGIN_FALLBACK_SHELL:-/bin/ash}" \
+    TZ="${TZ:-America/NewYork}" \
+    RUNNING_IN_DOCKER="true" \
+    DISTRIBUTION="$(uname -a)"
 
 # Add dependencies
-RUN apk add \ 
-        ca-certificates \
-        nano \
-        nano-syntax \
-        rsync \
-        curl \
-        wget \
-        jq \
-        yq
-    
-# Add optional corresponding documentation packages
-ARG NO_DOCS=
-ENV NO_DOCS="${NO_DOCS:-false}"
+RUN apk update && \
+    apk add \ 
+    zsh \
+    zsh-syntax-highlighting \
+    zsh-autosuggestions \
+    ca-certificates \
+    nano \
+    nano-syntax \
+    shadow \
+    rsync \
+    rsync-zsh-completion \
+    curl \
+    wget \
+    jq \
+    yq \
+    yq-zsh-completion
 
-RUN if [ "${NO_DOCS}" = "false" ]; then \
-        apk add \
-            man-pages \
-            man-db \
-            man-db-doc \
-            nano-doc \
-            curl-doc \
-            wget-doc \
-            jq-doc \
-            yq-doc; \
+# Add optional corresponding documentation packages
+RUN if [ "${NO_DOCS}" != "true" ]; then \
+    apk add \
+    man-pages \
+    man-db \
+    man-db-doc \
+    nano-doc \
+    curl-doc \
+    wget-doc \
+    jq-doc \
+    yq-doc; \
     fi
 
 RUN rm -rf /var/cache/apk/*
 
 FROM deps as zsh
 
-RUN apk update
-RUN apk add zsh
-
 RUN echo "# valid login shells" > /etc/shells && \
     echo "/bin/zsh" >> /etc/shells && \
     echo "/bin/bash" >> /etc/shells && \
     echo "/bin/ash" >> /etc/shells && \
-    echo "/bin/sh" >> /etc/shells
+    echo "/bin/sh" >> /etc/shells;
 
-RUN apk add \
-        zsh-syntax-highlighting \
-        shadow \
-        zsh-autosuggestions \
-        rsync-zsh-completion \
-        yq-zsh-completion
-
-RUN if [ "${NO_DOCS}" = "false" ]; then \
-        apk add \
-            zsh-doc \
-            zsh-syntax-highlighting-doc; \
+RUN if [ "${NO_DOCS}" != "true" ]; \ 
+    then \
+    apk add \
+    zsh-doc \
+    zsh-syntax-highlighting-doc; \
     fi
-    
+
 RUN rm -rf /var/cache/apk/*
 
 FROM zsh as ohmyzsh-install
 
-WORKDIR /root
+ARG NO_OHMYZSH=
+ENV NO_OHMYZSH="${NO_OHMYZSH:+true}"
 
-RUN apk add git
+COPY resources/tmp/docker-build/install-ohmyzsh.sh /tmp/docker-build/install-ohmyzsh.sh
 
-RUN /bin/zsh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-COPY resources/zshrc /root/.zshrc
+RUN if [ "${NO_OHMYZSH}" = "true" ]; then \
+    chmod +x /tmp/docker-build/install-ohmyzsh.sh && \
+    /tmp/docker-build/install-ohmyzsh.sh; \
+    fi
 
 FROM zsh as ohmyzsh
+ARG NO_OHMYZSH=
+ENV NO_OHMYZSH="${NO_OHMYZSH:+true}"
 
-COPY --from=ohmyzsh-install /root/.zshrc /root/.zshrc
-COPY --from=ohmyzsh-install /root/.oh-my-zsh /root/.oh-my-zsh
+COPY resources/tmp/docker-build/zshrc /tmp/docker-build/zshrc
+COPY 
 
 ENTRYPOINT [ "exec", "/bin/zsh" ]
 
@@ -110,11 +116,14 @@ ARG GLIBC_VERSION=
 ENV GLIBC_VERSION="${GLIBC_VERSION:-2.33-r0}"
 
 # Download and install glibc
-RUN curl -sSLo /etc/apk/keys/sgerrand.rsa.pub "https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub" && \
+RUN if [ "${GLIBC_VERSION}" != "none" ]; \
+    then \
+    curl -sSLo /etc/apk/keys/sgerrand.rsa.pub "https://alpine-pkgs.sgerrand.com/sgerrand.rsa.pub" && \
     curl -sSLo glibc.apk "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-${GLIBC_VERSION}.apk" && \
     curl -sSLo glibc-bin.apk "https://github.com/sgerrand/alpine-pkg-glibc/releases/download/${GLIBC_VERSION}/glibc-bin-${GLIBC_VERSION}.apk" && \
     apk add glibc-bin.apk glibc.apk && \
     /usr/glibc-compat/sbin/ldconfig /lib /usr/glibc-compat/lib && \
     echo 'hosts: files mdns4_minimal [NOTFOUND=return] dns mdns4' >> /etc/nsswitch.conf && \
     rm glibc.apk && \
-    rm glibc-bin.apk;
+    rm glibc-bin.apk; \
+    fi
--- build/docker-build.ps1
+++ build/docker-build.ps1
@@ -22,104 +22,104 @@
 ## OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 ## SOFTWARE.
 
-[CmdletBinding(SupportsShouldProcess=$true, DefaultParameterSetName="Build")]
+[CmdletBinding(SupportsShouldProcess = $true, DefaultParameterSetName = 'Build')]
 param(
     [Parameter(
-        Mandatory=$true,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage="Semantic version compliant string to tag built image with."
+        Mandatory = $true,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = 'Semantic version compliant string to tag built image with.'
     )]
-    [Alias("i")]
-    [Alias("-image-version")]
+    [Alias('i')]
+    [Alias('-image-version')]
     [ValidateNotNullOrEmpty]
     [string]$ImageVersion,
 
     [Parameter(
-        Mandatory=$false,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage="Semantic version compliant string that coincides with underlying base Alpine image. See dockerhub.com/alpine for values. 'latest' is considered valid.")]
-    [ALias("-alpine-version")]
-    [string]$AlpineVersion = "latest",
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = "Semantic version compliant string that coincides with underlying base Alpine image. See dockerhub.com/alpine for values. 'latest' is considered valid.")]
+    [ALias('-alpine-version')]
+    [string]$AlpineVersion = 'latest',
 
     [Parameter(
-        Mandatory=$false,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage="GLibC Version to install during build process. Empty value will assume latest version unless -ExcludeGLibC is specified.")]
-    [Alias("-glibc-version")]
-    [string]$GLibCVersion = "8.1_p1-r0",
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = 'GLibC Version to install during build process. Empty value will assume latest version unless -ExcludeGLibC is specified.')]
+    [Alias('-glibc-version')]
+    [string]$GLibCVersion = '8.1_p1-r0',
 
     [Parameter(
-        Mandatory=$false,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage="Flag indicating whether to include accompanying documentation packages. Including docs will increase image size significantly.")]
-    [Alias("-no-docs")]
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = 'Flag indicating whether to include accompanying documentation packages. Including docs will increase image size significantly.')]
+    [Alias('-no-docs')]
     [Flag]$NoDocs = $false,
 
     [Parameter(
-        Mandatory=$false,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage="Flag indicating whether glibc should be excluded entirely. Presence of this flag overrides version specified in -g | [--no-glibc]. Including glibc will increase image size significantly.")]
-    [Alias("-no-glibc")]
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = 'Flag indicating whether glibc should be excluded entirely. Presence of this flag overrides version specified in -g | [--no-glibc]. Including glibc will increase image size significantly.')]
+    [Alias('-no-glibc')]
     [Flag]$NoGLibC = $false,
 
     [Parameter(
-        Mandatory=$false,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage=("Registry which the image will be pushed upon successful build. If not using dockerhub, the full FQDN must be specified. " +
-                    "This assumes the default docker daemon is already authenticated with the registry specified. If dockerhub is used, just the username is required. " +
-                    "Default value: jessenich91."))]
-    [Alias("-registry")]
-    [string]$Registry = "jessenich91",
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = ('Registry which the image will be pushed upon successful build. If not using dockerhub, the full FQDN must be specified. ' +
+            'This assumes the default docker daemon is already authenticated with the registry specified. If dockerhub is used, just the username is required. ' +
+            'Default value: jessenich91.'))]
+    [Alias('-registry')]
+    [string]$Registry = 'jessenich91',
 
     [Parameter(
-        Mandatory=$false,
-        ValueFromPipeline=$false,
-        ValueFromPipelineByPropertyName=$false,
-        HelpMessage="Repository which the image will be pushed upon successful build. Default value: 'alpine-zsh'")]
-    [Alias("-repository")]
-    [string]$Repository = "alpine-zsh"
+        Mandatory = $false,
+        ValueFromPipeline = $false,
+        ValueFromPipelineByPropertyName = $false,
+        HelpMessage = "Repository which the image will be pushed upon successful build. Default value: 'alpine-zsh'")]
+    [Alias('-repository')]
+    [string]$Repository = 'alpine-zsh'
 )
 
 begin {
     if ([string]::IsNullOrWhiteSpace($ImageVersion)) {
-        throw [System.ArgumentNullException]::new("ImageVersion");
+        throw [System.ArgumentNullException]::new('ImageVersion');
     }
 
     if ($NoGLibC -and -not [string]::IsNullOrWhiteSpace($GLibCVersion)) {
         $GLibCVersion = $null;
     }
 
-    $Script:Tag1 = "latest";
+    $Script:Tag1 = 'latest';
     $Script:Tag2 = $ImageVersion;
-    $Script:RepositoryRoot = ".";
+    $Script:RepositoryRoot = '.';
 }
 
 process {
-    if ($GLibCVersion -ne "none") {
-        $targetStage = "glibc";
+    if ($GLibCVersion -ne 'none') {
+        $targetStage = 'glibc';
 
         if ($NoDocs) {
-            $Script:Tag1 = "no-docs-glibc-latest";
+            $Script:Tag1 = 'no-docs-glibc-latest';
             $Script:Tag2 = "no-docs-glibc-$ImageVersion";
         }
         else {
-            $Script:Tag1 = "latest";
+            $Script:Tag1 = 'latest';
             $Script:Tag2 = "$ImageVersion";
         }
     }
     else {
         if ($NoDocs) {
-            $Script:Tag1 = "no-docs-latest";
+            $Script:Tag1 = 'no-docs-latest';
             $Script:Tag2 = "no-docs-$ImageVersion";
         }
         else {
-            $Script:Tag1 = "no-glibc-latest";
+            $Script:Tag1 = 'no-glibc-latest';
             $Script:Tag2 = "no-glibc-$ImageVersion";
         }
     }
@@ -130,7 +130,7 @@ process {
         -t "$($Registry)/$($Repository):$($Script:Tag2)" `
         --build-arg "ALPINE_VERSION=$AlpineVersion" `
         --build-arg "NO_DOCS=$NO_DOCS" `
-        --platform linux/arm/v7,linux/arm64/v8,linux/amd64 `
+        --platform linux/arm/v7, linux/arm64/v8, linux/amd64 `
         --target $targetStage `
         --push `
         $Script:RepositoryRoot
