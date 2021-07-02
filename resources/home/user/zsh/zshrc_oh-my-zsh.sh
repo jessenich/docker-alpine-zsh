ebcb4edcca2f0c07fdb68d82115586ca450e2e07
Jesse N <jesse@keplerdev.com>
2021-07-02 10:26:44 -0400

    Added bake file

--- resources/home/user/zsh/zshrc_oh-my-zsh.sh
+++ resources/home/user/zsh/zshrc_oh-my-zsh.sh
@@ -1,14 +1,17 @@
+# Copyright (c) 2021 Jesse N. <jesse@keplerdev.com>
+# This work is licensed under the terms of the MIT license. For a copy, see <https://opensource.org/licenses/MIT>.
+
 # If you come from bash you might have to change your $PATH.
 # export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Path to your oh-my-zsh installation.
-export ZSH="/root/.oh-my-zsh"
+export ZSH="/home/${USER}/.oh-my-zsh"
 
 # Set name of the theme to load --- if set to "random", it will
 # load a random theme each time oh-my-zsh is loaded, in which case,
 # to know which specific one was loaded, run: echo $RANDOM_THEME
 # See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-export ZSH_THEME="rkj-repos"
+export ZSH_THEME="agnoster"
 
 # Set list of themes to pick from when loading at random
 # Setting this variable when ZSH_THEME=random will cause zsh to load
@@ -104,5 +107,4 @@ export LANG=en_US.UTF-8
 #
 # Example aliases
 
-source /usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
--- resources/home/zsh/zsh_env.sh
+++
@@ -1,6 +0,0 @@
-######
-### Custom environment settings
-######
-
-export FUNCTIONS_CORE_TOOLS_TELEMETRY_OPTOUT=1
-export RUNNING_IN_DOCKER="true"
